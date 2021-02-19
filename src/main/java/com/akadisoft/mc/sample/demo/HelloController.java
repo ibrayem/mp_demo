251bef3e5365d2ed7d9f5db17519a996c610be9b
Mohammad Amin <ibrayem@gmail.com>
2021-02-19 01:02:08 -0500

    Adding sources

---
+++ src/main/java/com/akadisoft/mc/sample/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.akadisoft.mc.sample.demo;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
